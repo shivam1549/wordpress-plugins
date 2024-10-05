9ea77548b485f533fc8c00936d184f93934ed10c
22-shivam <121663716+22-shivam@users.noreply.github.com>
2024-10-05 07:59:19 +0530

    New features added

--- Custom Product Attributes/assets/js/fabric-pricing.js
+++ Custom Product Attributes/assets/js/fabric-pricing.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     // Set initial values based on the first option selected
     var $widthSelect = $('#fabric-width');
     var $rateDisplay = $('#fabric-rate');
@@ -11,12 +11,12 @@ jQuery(document).ready(function($) {
         var rate = parseFloat(selectedOption.data('rate'));
         var minLength = parseFloat(selectedOption.data('min-length'));
         var minPrice = parseFloat(selectedOption.data('min-price'));
-        
+
         // Update the display fields
         $rateDisplay.text(rate + ' ' + fabricPricingData.currency); // Add currency symbol
         $minLengthDisplay.text(minLength + ' yards');
         $minPriceDisplay.text(minPrice + ' ' + fabricPricingData.currency); // Add currency symbol
-        
+
         calculateTotalPrice();
     }
 
@@ -24,39 +24,103 @@ jQuery(document).ready(function($) {
         var selectedOption = $widthSelect.find('option:selected');
         var rate = parseFloat(selectedOption.data('rate'));
         var length = parseFloat($('#fabric-length').val());
-        
+        var minprice = parseFloat(selectedOption.data('min-price'));
+
         if (!isNaN(length) && length > 0) {
             var totalPrice = rate * length;
-             
-        $('.extra-option:checked').each(function() {
-            let percentage = parseFloat($(this).data('price-increase'));
-            totalPrice += (totalPrice * (percentage / 100));
-        });
+            console.log(totalPrice + " tot " + " min " + minprice)
+            if(totalPrice < minprice){
+                totalPrice = minprice;
+            }
+            var optionprice = 0;
+            $('.extra-option:checked').each(function () {
+                let percentage = parseFloat($(this).data('price-increase'));
+                optionprice += (totalPrice * (percentage / 100));
+            });
+            totalPrice = totalPrice + optionprice;
             $totalPriceDisplay.text(totalPrice.toFixed(2) + ' ' + fabricPricingData.currency); // Add currency symbol
-            
+
         } else {
             $totalPriceDisplay.text('0.00 ' + fabricPricingData.currency); // Add currency symbol
         }
     }
 
     // On width change, update fabric details
-    $widthSelect.change(function() {
+    $widthSelect.change(function () {
         updateFabricDetails();
     });
 
     // On length input, recalculate total price
-    $('#fabric-length').on('input', function() {
+    $('#fabric-length').on('input', function () {
+        var selectedOption = $widthSelect.find('option:selected');
+        var minlength = parseFloat(selectedOption.data('min-length'));
+        var selectedlength = parseFloat($(this).val())
+        if(selectedlength < minlength){
+            alert("Please select a minimum of " + minlength + " yards");
+            return;
+        }
         calculateTotalPrice();
     });
 
     // Initialize on page load
     updateFabricDetails();
 
-    $('.extra-option').on('change', function() {
+    $('.extra-option').on('change', function () {
 
         calculateTotalPrice();
-       
+
     });
+
+    loadFabricWeights();
+
+    function loadFabricWeights() {
+        var fabricArray = fabricPricingDataarray.fabricdata;
+
+        var filterednewarray = [];
+        if (fabricArray.length > 0) {
+            filterednewarray.push(fabricArray[0].weight);
+        }
+
+        fabricArray.forEach(element => {
+            //    console.log(element.weight)
+            if (!filterednewarray.includes(element.weight)) {
+                filterednewarray.push(element.weight);
+            }
+        });
+        var fabricweighthtml = `<label>Fabric Weight</label><select name="fabric_weight" id="fabricweightselect">`;
+        filterednewarray.forEach(fabricweight => {
+            fabricweighthtml += `<option value="${fabricweight}">${fabricweight}</option>`
+        })
+        fabricweighthtml += `</select>`
+
+        $("#fabricweight").html(fabricweighthtml);
+
+        $("#fabricweightselect").on('change', function () {
+            $("#fabric-width").html();
+            var selectedWieght = $(this).val();
+            console.log(selectedWieght + " hey")
+            var widths = fabricArray.filter(fabric => fabric.weight === selectedWieght);
+            // console.log(widths);
+            var fabricwidthhtml = '';
+            if(widths){
+                widths.forEach(fabricwidth=>{
+                    fabricwidthhtml += `<option
+                    data-rate="${fabricwidth.rate}"
+                    data-min-length="${fabricwidth.min_length}"
+                    data-min-price="${fabricwidth.min_price}"
+                    value="${fabricwidth.width}">${fabricwidth.width}</option>`
+                })
+
+                $("#fabric-width").html(fabricwidthhtml);
+            }
+        })
+
+
+    }
+
+
+
 });
+
 
 
